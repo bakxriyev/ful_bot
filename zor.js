589cb6a617704626019480e9c45f73cdfffebd11
bakxriyev <kamronbekbahriyev18@gmail.com>
2026-04-30 18:00:07 +0500

    dsds

--- zor.js
+++ zor.js
@@ -9,7 +9,7 @@ const path = require('path');
 // ========================
 // KONFIGURATSIYA
 // ========================
-const BOT_TOKEN    = '8536141927:AAF9dooLSHGxZRBLqV_o3uAPsIZ9jl5v6Js';
+const BOT_TOKEN    = '8679225810:AAG-wo6seWBQTAxBkfb0rD1c0L0Kdul49fE';
 const CHANNEL_ID   = '-1003920803109';
 const SUPABASE_URL = 'https://vxpvgeyktgyasegvycfp.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_pXpHGuZFzmhJUD6FkQeapQ__7D78i4w';
